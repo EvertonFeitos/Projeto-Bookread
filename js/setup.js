6a2b1c98a03e615f602b2cae2dd53b3de9c36e1c
Everton <feitosasantoseverton@gmail.com>
2022-04-26 18:20:11 -0300

    Projeto adicionado ao Github

---
+++ js/setup.js
@@ -0,0 +1,41 @@
+/* funcionamento modal */
+function openModal(mn) {
+    let modal = document.getElementById(mn);
+
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+
+    modal.style.display = 'Block';
+    document.body.style.overflow = 'hidden';
+}
+
+function closeModal(mn) {
+    let modal = document.getElementById(mn);
+
+    if (typeof modal == 'undefined' || modal === null)
+        return;
+
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+}
+
+
+/*carrosel em jquery */
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
